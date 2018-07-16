822864496539f55b4d47573e47783f4d452a2bb6
cazdemun <cplu1509@gmail.com>
2018-07-15 21:12:24 -0500

    v0.0.1
    You can save the file
    You can load another file
    
    gojs do most of the work tbh

--- src/SideMenu/SideMenu.js
+++ src/SideMenu/SideMenu.js
@@ -7,21 +7,17 @@ const sideMenu = (props) => {
             <li className="list-group-item col-md-12">
                 <p>Options</p>
             </li>
-            <div className="row">
-                <li className="list-group-item col-md-6 col-lg-6 col-xl-6">
-                    <input type="file" accept=".json" id="fileInput" aria-describedby="fileHelp"
-                    onChange={props.submitted} />
-                </li>
-                <li className="list-group-item col-md-6 col-lg-6 col-xl-6">
-                    <button onClick={props.saveClicked}>Save</button>
-                </li>
-                <li className="list-group-item col-md-6 col-lg-6 col-xl-6">
-                    <input style={{width: 'parent'}}/>
-                </li>
-                <li className="list-group-item col-md-6 col-lg-6 col-xl-6">
-                    <button onClick={props.addClicked}>Add</button>
-                </li>
-            </div>
+
+            <li className="list-group-item col-md-12">
+                <input type="file" accept=".json" id="fileInput" aria-describedby="fileHelp"
+                style={{width:'parent'}}
+                onChange={props.submitted} />
+            </li>
+
+            <li className="list-group-item col-md-12">
+                <button onClick={props.saveClicked}>Save</button>
+            </li>
+
             <li className="list-group-item col-md-12">
                 <p>Files</p>
             </li>
